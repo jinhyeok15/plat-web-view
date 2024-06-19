1675c7166ec04d35791d1cfd304c0e90207b3842
jinhyeok15 <ekfrhd97@gmail.com>
2024-06-20 00:36:34 +0900

    RestAPI getClient method 추가

--- usecase/api/index.ts
+++ usecase/api/index.ts
@@ -1,4 +1,5 @@
-import { RestAPI } from '@modules/fetch'
+import { RestAPI as _RestAPI } from '@modules/fetch'
+import { FetchConfig } from '@modules/fetch/api'
 import { URI } from '@plat/api/resource'
 import { getProcessEnv } from '@plat/server/Environment'
 
@@ -6,7 +7,17 @@ const processEnv = getProcessEnv()
 
 const BASE_URL = processEnv.PLAT_API_URL as string
 
-const api = new RestAPI<URI>(BASE_URL, {
+class RestAPI extends _RestAPI<URI> {
+  constructor(baseUrl: string, init?: Partial<FetchConfig<URI>>) {
+    super(baseUrl, init)
+  }
+
+  getClient(config?: Partial<FetchConfig<URI>>) {
+    return new _RestAPI(BASE_URL, config)
+  }
+}
+
+const api = new RestAPI(BASE_URL, {
   credentials: 'include',
 })
 
