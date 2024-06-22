cdd7a387628a889e0bd7696e3d15c5741cf1aba5
jinhyeok15 <ekfrhd97@gmail.com>
2024-06-22 15:52:57 +0900

    KAKAO_REST_API_KEY 환경변수 세팅

--- usecase/server/Environment.tsx
+++ usecase/server/Environment.tsx
@@ -1,6 +1,7 @@
 export const getProcessEnv = () => ({
   KAKAO_JAVASCRIPT_APP_KEY: process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_APP_KEY,
   PLAT_API_URL: process.env.PLAT_API_URL,
+  KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY,
 })
 
 const Environment = ({ children }: { children: React.ReactNode }) => {
